6d02c565d65e8fa17b9a9f071c57d8fe73c2e150
Roni Sarkar <ronichandrasarkar@gmail.com>
2025-07-28 11:50:56 +0530

    added seo meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,27 @@ import TopHeader from "@/components/header";
 import HomeComponent from "@/components/home.component";
 import { getProductData } from "@/lib/api";
 import { ProductData } from "@/lib/model";
+import { Metadata } from "next";
+
+// for seo metadata
+export async function generateMetadata(): Promise<Metadata> {
+  const productData: ProductData = await getProductData();
+  return {
+    title: productData.seo.title,
+    description: productData.seo.description,
+    keywords: productData.seo.keywords,
+    openGraph: {
+      title: productData.seo.title,
+      description: productData.seo.description,
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: productData.seo.title,
+      description: productData.seo.description,
+    },
+  };
+}
 
 export default async function Home() {
   const productData: ProductData = await getProductData();
